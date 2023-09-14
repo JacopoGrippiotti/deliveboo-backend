475837ffb92c25728548c314e58ca4b1f26862a9
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-14 19:05:57 +0200

    removed ingredient column in dishes table

--- database/migrations/2023_09_12_143716_create_dishes_table.php
+++ database/migrations/2023_09_12_143716_create_dishes_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('ingredients');
             $table->text('description');
             $table->double('price', 5, 2);
             $table->string('course');
