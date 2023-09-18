d74344d48bb858ed21f01b92ed9705c161e134b2
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-18 11:47:44 +0200

    added api routes for DishesControler

--- routes/api.php
+++ routes/api.php
@@ -35,16 +35,16 @@ Route::name('api.admin.')->middleware('auth')->group(function () {
     Route::delete('{user}/restaurants/deleted/{restaurant}', [ApiAdminController::class, 'restore'])->name('restore.restaurant');
     Route::delete('{user}/restaurants/deleted/{restaurant}/hardDelete', [ApiAdminController::class, 'obliterate'])->name('obliterate.restaurants');
 
-    Route::get('/{user}/dishes', [ApiDishController::class, 'index'])->name('index.restaurants');
-    Route::get('/{user}/dishes/{dish}', [ApiDishController::class, 'show'])->name('show.restaurants');
-    Route::get('/{user}/dishes/create', [ApiDishController::class, 'create'])->name('create.restaurants');
-    Route::post('/{user}/dishes', [ApiDishController::class, 'store'])->name('store.restaurants');
-    Route::get('/{user}/dishes/{dish}/edit', [ApiDishController::class, 'edit'])->name('edit.restaurants');
-    Route::put('/{user}/dishes/{dish}', [ApiDishController::class, 'update'])->name('update.restaurants');
-    Route::delete('/{user}/dishes/{dish}', [ApiDishController::class, 'delete'])->name('destroy.restaurants');
-    Route::get('{user}/dishes/deleted', [ApiDishController::class, 'deletedIndex'])->name('trashed.restaurants');
-    Route::delete('{user}/dishes/deleted/{dish}', [ApiDishController::class, 'restore'])->name('restore.restaurant');
-    Route::delete('{user}/dishes/deleted/{dish}/hardDelete', [ApiDishController::class, 'obliterate'])->name('obliterate.restaurants');
+    Route::get('/{user}/dishes', [ApiDishController::class, 'index'])->name('index.dishes');
+    Route::get('/{user}/dishes/{dish}', [ApiDishController::class, 'show'])->name('show.dishes');
+    Route::get('/{user}/dishes/create', [ApiDishController::class, 'create'])->name('create.dishes');
+    Route::post('/{user}/dishes', [ApiDishController::class, 'store'])->name('store.dishes');
+    Route::get('/{user}/dishes/{dish}/edit', [ApiDishController::class, 'edit'])->name('edit.dishes');
+    Route::put('/{user}/dishes/{dish}', [ApiDishController::class, 'update'])->name('update.dishes');
+    Route::delete('/{user}/dishes/{dish}', [ApiDishController::class, 'delete'])->name('destroy.dishes');
+    Route::get('{user}/dishes/deleted', [ApiDishController::class, 'deletedIndex'])->name('trashed.dishes');
+    Route::delete('{user}/dishes/deleted/{dish}', [ApiDishController::class, 'restore'])->name('restore.dish');
+    Route::delete('{user}/dishes/deleted/{dish}/hardDelete', [ApiDishController::class, 'obliterate'])->name('obliterate.dishes');
 });
 
 
