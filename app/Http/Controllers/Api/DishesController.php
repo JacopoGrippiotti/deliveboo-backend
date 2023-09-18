b345f7d1bb77d035cda782ef8a3e249c128c87e9
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-18 13:17:22 +0200

    added Ingredient Model in DishesController

--- app/Http/Controllers/Api/DishesController.php
+++ app/Http/Controllers/Api/DishesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Restaurant;
+use App\Models\Ingredient;
 
 class DishesController extends Controller
 {
