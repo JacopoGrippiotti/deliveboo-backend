adb6102b76125b53ad8c1bc67c1148d794a53944
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-18 17:31:10 +0200

    added SoftDeletes to Dish Model

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Dish extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
+    
     protected $casts = [
         'ingredients' => 'array'
     ];
