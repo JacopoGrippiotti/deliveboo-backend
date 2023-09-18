b5f080270bc8765b96a7aef86bce2121874c9d0f
manuelcamaioni <camaioni.manuel@gmail.com>
2023-09-18 18:03:58 +0200

    added orders crud

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -24,12 +24,28 @@ class OrdersController extends Controller
             'results' => $order
         ]);
     }
-    public function store(Request $request){
+    public function store(Request $request, Restaurant $restaurant){
         $data = $request->validate([
-
+            'total_price' => ['required', 'decimal:2'],
+            'customer_name' => ['required', 'string'],
+            'customer_address' => ['required', 'string'],
+            'phone_number' => ['required', 'string'],
         ]);
+        $data['status'] = 0;
+        $data['restaurant_id'] = $restaurant->id;
+        $newOrder = Order::create($data);
+        $dishes = $request->dishes;
+        $dishesIds = Dish::whereIn('name', $dishes)->pluck('id')->toArray();
+        $newOrder->save();
+        $newOrder->dishes()->sync($dishesIds);
     }
-    public function destroy(){
-
+    public function destroy($orderId){
+        $order = Order::findOrFail($orderId);
+        if (!$order) {
+            return redirect()->back()->with('error', 'Ordine non trovato.');
+        }
+        $order->dishes()->detach();
+        $order->delete();
+        return redirect()->back()->with('success', 'Ordine eliminato con successo.');
     }
 }
